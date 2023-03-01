10885495ed5348232ae297a4a6d7901685231a99
Monurh <74192667+Monurh@users.noreply.github.com>
2023-03-01 10:02:20 +0200

    Add files via upload

---
+++ lab 6/WindowsFormsApp2/WindowsFormsApp2/Flyweight.cs
@@ -0,0 +1,44 @@
+﻿using System.Drawing;
+
+
+namespace FlyWeight
+{
+    public class Param
+    {
+        public int h;
+        public Bitmap pic;
+    }
+
+    public class Snowflake1 : Param
+    {
+        public Snowflake1()
+        {
+            h = 50;
+            pic = new Bitmap(@"D:\4 курс\лаб\lab 6\1.png");
+        }
+    }
+    public class Snowflake2 : Param
+    {
+        public Snowflake2()
+        {
+            h = 100;
+            pic = new Bitmap(@"D:\4 курс\лаб\lab 6\2.jpg");
+        }
+    }
+    public class Snowflake
+    {
+        public int x, y;
+        public Param p;
+        public Snowflake()
+        {
+            x = 0; y = 0;
+        }
+      
+        public Snowflake(int x, int y, Param p)
+        {
+            this.p = p;
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
---
+++ lab 6/WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlyWeight
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            g = this.CreateGraphics();
+        }
+        Graphics g;
+        Snowflake[] t = new Snowflake[500];
+
+        Param[] p = new Param[2];
+        Snowflake1 p1 = new Snowflake1();
+        Snowflake2 p2 = new Snowflake2();
+        int count = 0;
+        Random r = new Random();
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+
+            if (r.Next(0, 10) % 2 == 0)
+                t[count] = new Snowflake(r.Next(10, 1500), r.Next(10, 1500), p1);
+            else
+                t[count] = new Snowflake(r.Next(10, 1500), r.Next(10, 1500), p2);
+
+            g.DrawImage(t[count].p.pic, new Rectangle(t[count].x, t[count].y, t[count].p.h, 20));
+            if (count < 499)
+                count++;
+            else
+                timer1.Stop();
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+    }
+}
